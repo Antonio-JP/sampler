b471fb7e8faa80b9441cd7c24fd62ae15c39e487
Rioukay <kethewal@gmail.com>
2016-06-30 16:35:06 +0200

    Added a JUnit Test
    Added a JUnit test that was not included previously.
    
    Update Site updated

---
+++ rioko.sampler.directoryDrawer/test/rioko/sampler/directoryDrawer/junit/AllDirectoryTests.java
@@ -0,0 +1,11 @@
+package rioko.sampler.directoryDrawer.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({CreateDiagramPerformance.class, GetFilesPerformance.class})
+public class AllDirectoryTests {
+
+}
